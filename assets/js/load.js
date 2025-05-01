34ae07191ec91c27340f9a498e78ee0eef2584ae
Aspasija <aspasija.cvetkoska@result.si>
2025-05-01 13:32:42 +0200

    added loader

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,352 +1,372 @@
-(function (e, t) {
+(function (global, undefined) {
   "use strict";
-  function m() {}
-  function g(e, t) {
-    if (!e) {
-      return;
+
+  // No-operation function
+  function noop() {}
+
+  // Iterate over an array-like structure
+  function each(collection, callback) {
+    if (!collection) return;
+    if (typeof collection === "object") {
+      collection = [].slice.call(collection);
     }
-    if (typeof e === "object") {
-      e = [].slice.call(e);
-    }
-    for (var n = 0, r = e.length; n < r; n++) {
-      t.call(e, e[n], n);
+    for (let i = 0, len = collection.length; i < len; i++) {
+      callback.call(collection, collection[i], i);
     }
   }
-  function y(e, n) {
-    var r = Object.prototype.toString.call(n).slice(8, -1);
-    return n !== t && n !== null && r === e;
+
+  // Type checking utility
+  function isType(type, obj) {
+    return obj !== undefined && obj !== null &&
+           Object.prototype.toString.call(obj).slice(8, -1) === type;
   }
-  function b(e) {
-    return y("Function", e);
+
+  function isFunction(obj) {
+    return isType("Function", obj);
   }
-  function w(e) {
-    return y("Array", e);
+
+  function isArray(obj) {
+    return isType("Array", obj);
   }
-  function E(e) {
-    var t = e.split("/"),
-      n = t[t.length - 1],
-      r = n.indexOf("?");
-    return r !== -1 ? n.substring(0, r) : n;
+
+  // Extracts file name from URL
+  function getFileName(url) {
+    let parts = url.split("/");
+    let last = parts[parts.length - 1];
+    let queryIndex = last.indexOf("?");
+    return queryIndex !== -1 ? last.substring(0, queryIndex) : last;
   }
-  function S(e) {
-    e = e || m;
-    if (e._done) {
-      return;
-    }
-    e();
-    e._done = 1;
+
+  function done(callback) {
+    if (callback._done) return;
+    callback();
+    callback._done = true;
   }
-  function x(e, t, n, r) {
-    var i =
-      typeof e === "object"
-        ? e
-        : {
-            test: e,
-            success: !!t ? (w(t) ? t : [t]) : false,
-            failure: !!n ? (w(n) ? n : [n]) : false,
-            callback: r || m,
-          };
-    var s = !!i.test;
-    if (s && !!i.success) {
-      i.success.push(i.callback);
-      c.load.apply(null, i.success);
-    } else if (!s && !!i.failure) {
-      i.failure.push(i.callback);
-      c.load.apply(null, i.failure);
+
+  // Conditional loader
+  function test(condition, success, failure, callback) {
+    const obj = typeof condition === "object" ? condition : {
+      test: condition,
+      success: success ? (isArray(success) ? success : [success]) : false,
+      failure: failure ? (isArray(failure) ? failure : [failure]) : false,
+      callback: callback || noop,
+    };
+
+    const result = !!obj.test;
+
+    if (result && obj.success) {
+      obj.success.push(obj.callback);
+      loader.load.apply(null, obj.success);
+    } else if (!result && obj.failure) {
+      obj.failure.push(obj.callback);
+      loader.load.apply(null, obj.failure);
     } else {
-      r();
+      obj.callback();
     }
-    return c;
+
+    return loader;
   }
-  function T(e) {
-    var t = {};
-    if (typeof e === "object") {
-      for (var n in e) {
-        if (!!e[n]) {
-          t = { name: n, url: e[n] };
+
+  // Normalize resource definition
+  function normalizeResource(resource) {
+    let result = {};
+    if (typeof resource === "object") {
+      for (let name in resource) {
+        if (resource[name]) {
+          result = { name, url: resource[name] };
         }
       }
     } else {
-      t = { name: E(e), url: e };
+      result = { name: getFileName(resource), url: resource };
     }
-    var r = o[t.name];
-    if (r && r.url === t.url) {
-      return r;
+
+    if (resources[result.name] && resources[result.name].url === result.url) {
+      return resources[result.name];
     }
-    o[t.name] = t;
-    return t;
+
+    resources[result.name] = result;
+    return result;
   }
-  function N(e) {
-    e = e || o;
-    for (var t in e) {
-      if (e.hasOwnProperty(t) && e[t].state !== v) {
+
+  function allLoaded(scope = resources) {
+    for (let key in scope) {
+      if (scope.hasOwnProperty(key) && scope[key].state !== STATE.LOADED) {
         return false;
       }
     }
     return true;
   }
-  function C(e) {
-    e.state = p;
-    g(e.onpreload, function (e) {
-      e.call();
-    });
+
+  function preloadDone(resource) {
+    resource.state = STATE.PRELOADED;
+    each(resource.onpreload, (fn) => fn());
   }
-  function k(e, n) {
-    if (e.state === t) {
-      e.state = h;
-      e.onpreload = [];
-      M({ url: e.url, type: "cache" }, function () {
-        C(e);
-      });
+
+  function preload(resource) {
+    if (resource.state === undefined) {
+      resource.state = STATE.PRELOADING;
+      resource.onpreload = [];
+      loadResource({ url: resource.url, type: "cache" }, () => preloadDone(resource));
     }
   }
-  function L() {
-    var e = arguments,
-      t = [].slice.call(e, 1),
-      n = t[0];
-    if (!a) {
-      i.push(function () {
-        c.load.apply(null, e);
-      });
-      return c;
+
+  // Load sequence logic
+  function load() {
+    const args = arguments;
+    const rest = [].slice.call(args, 1);
+    const first = args[0];
+
+    if (!initialized) {
+      preloadQueue.push(() => loader.load.apply(null, args));
+      return loader;
     }
-    if (!!n) {
-      g(t, function (e) {
-        if (!b(e) && !!e) {
-          k(T(e));
+
+    if (rest.length) {
+      each(rest, (res) => {
+        if (!isFunction(res) && res) {
+          preload(normalizeResource(res));
         }
       });
-      O(
-        T(e[0]),
-        b(n)
-          ? n
-          : function () {
-              c.load.apply(null, t);
-            }
+
+      loadResourceWithCallback(
+        normalizeResource(first),
+        isFunction(rest[0])
+          ? rest[0]
+          : () => loader.load.apply(null, rest)
       );
     } else {
-      O(T(e[0]));
+      loadResourceWithCallback(normalizeResource(first));
     }
-    return c;
+
+    return loader;
   }
-  function A() {
-    var e = arguments,
-      t = e[e.length - 1],
-      n = {};
-    if (!b(t)) {
-      t = null;
-    }
-    g(e, function (r, i) {
-      if (r !== t) {
-        r = T(r);
-        n[r.name] = r;
-        O(
-          r,
-          t && i === e.length - 2
-            ? function () {
-                if (N(n)) {
-                  S(t);
-                }
-              }
-            : null
-        );
+
+  // Load multiple resources in parallel
+  function loadMultiple() {
+    const args = arguments;
+    const lastArg = args[args.length - 1];
+    let callback = isFunction(lastArg) ? lastArg : null;
+    const resourceMap = {};
+
+    each(args, (res, idx) => {
+      if (res !== callback) {
+        res = normalizeResource(res);
+        resourceMap[res.name] = res;
+        loadResourceWithCallback(res, callback && idx === args.length - 2 ? () => {
+          if (allLoaded(resourceMap)) done(callback);
+        } : null);
       }
     });
-    return c;
+
+    return loader;
   }
-  function O(e, t) {
-    t = t || m;
-    if (e.state === v) {
-      t();
+
+  function loadResourceWithCallback(resource, callback = noop) {
+    if (resource.state === STATE.LOADED) {
+      callback();
       return;
     }
-    if (e.state === d) {
-      c.ready(e.name, t);
+
+    if (resource.state === STATE.LOADING) {
+      loader.ready(resource.name, callback);
       return;
     }
-    if (e.state === h) {
-      e.onpreload.push(function () {
-        O(e, t);
-      });
+
+    if (resource.state === STATE.PRELOADING) {
+      resource.onpreload.push(() => loadResourceWithCallback(resource, callback));
       return;
     }
-    e.state = d;
-    M(e, function () {
-      e.state = v;
-      t();
-      g(s[e.name], function (e) {
-        S(e);
-      });
-      if (f && N()) {
-        g(s.ALL, function (e) {
-          S(e);
-        });
+
+    resource.state = STATE.LOADING;
+
+    loadResource(resource, () => {
+      resource.state = STATE.LOADED;
+      callback();
+      each(readyCallbacks[resource.name], done);
+      if (domReady && allLoaded()) {
+        each(readyCallbacks.ALL, done);
       }
     });
   }
-  function M(t, r) {
-    function i(t) {
-      t = t || e.event;
-      o.onload = o.onreadystatechange = o.onerror = null;
-      r();
-    }
-    function s(t) {
-      t = t || e.event;
-      if (
-        t.type === "load" ||
-        (/loaded|complete/.test(o.readyState) &&
-          (!n.documentMode || n.documentMode < 9))
-      ) {
-        o.onload = o.onreadystatechange = o.onerror = null;
-        r();
-      }
-    }
-    r = r || m;
-    var o;
-    if (/\.css[^\.]*$/.test(t.url)) {
-      o = n.createElement("link");
-      o.type = "text/" + (t.type || "css");
-      o.rel = "stylesheet";
-      o.href = t.url;
+
+  function loadResource(resource, callback = noop) {
+    let element;
+    const isCSS = /\.css[^.]*$/.test(resource.url);
+
+    if (isCSS) {
+      element = document.createElement("link");
+      element.type = "text/css";
+      element.rel = "stylesheet";
+      element.href = resource.url;
     } else {
-      o = n.createElement("script");
-      o.type = "text/" + (t.type || "javascript");
-      o.src = t.url;
+      element = document.createElement("script");
+      element.type = "text/javascript";
+      element.src = resource.url;
     }
-    o.onload = o.onreadystatechange = s;
-    o.onerror = i;
-    o.async = false;
-    o.defer = false;
-    var u = n.head || n.getElementsByTagName("head")[0];
-    u.insertBefore(o, u.lastChild);
+
+    element.onload = element.onreadystatechange = function (event) {
+      if (
+        event.type === "load" ||
+        (/loaded|complete/.test(element.readyState) && (!document.documentMode || document.documentMode < 9))
+      ) {
+        element.onload = element.onreadystatechange = element.onerror = null;
+        callback();
+      }
+    };
+
+    element.onerror = () => {
+      element.onload = element.onreadystatechange = element.onerror = null;
+      callback();
+    };
+
+    element.async = false;
+    element.defer = false;
+
+    const head = document.head || document.getElementsByTagName("head")[0];
+    head.insertBefore(element, head.lastChild);
   }
-  function _() {
-    var e = n.getElementsByTagName("script");
-    for (var t = 0, r = e.length; t < r; t++) {
-      var i = e[t].getAttribute("data-headjs-load");
-      if (!!i) {
-        c.load(i);
+
+  function checkAutoload() {
+    const scripts = document.getElementsByTagName("script");
+    for (let i = 0; i < scripts.length; i++) {
+      const dataLoad = scripts[i].getAttribute("data-headjs-load");
+      if (dataLoad) {
+        loader.load(dataLoad);
         return;
       }
     }
   }
-  function D(e, t) {
-    if (e === n) {
-      if (f) {
-        S(t);
+
+  function onReady(resourceName, callback) {
+    if (resourceName === document) {
+      if (domReady) {
+        done(callback);
       } else {
-        r.push(t);
+        domReadyCallbacks.push(callback);
       }
-      return c;
+      return loader;
     }
-    if (b(e)) {
-      t = e;
-      e = "ALL";
+
+    if (isFunction(resourceName)) {
+      callback = resourceName;
+      resourceName = "ALL";
     }
-    if (typeof e !== "string" || !b(t)) {
-      return c;
+
+    if (typeof resourceName !== "string" || !isFunction(callback)) {
+      return loader;
     }
-    var i = o[e];
-    if ((i && i.state === v) || (e === "ALL" && N() && f)) {
-      S(t);
-      return c;
-    }
-    var u = s[e];
-    if (!u) {
-      u = s[e] = [t];
+
+    const res = resources[resourceName];
+
+    if ((res && res.state === STATE.LOADED) || (resourceName === "ALL" && allLoaded() && domReady)) {
+      done(callback);
     } else {
-      u.push(t);
+      readyCallbacks[resourceName] = readyCallbacks[resourceName] || [];
+      readyCallbacks[resourceName].push(callback);
     }
-    return c;
+
+    return loader;
   }
-  function P() {
-    if (!n.body) {
-      e.clearTimeout(c.readyTimeout);
-      c.readyTimeout = e.setTimeout(P, 50);
+
+  function onDomReady() {
+    if (!document.body) {
+      clearTimeout(loader.readyTimeout);
+      loader.readyTimeout = setTimeout(onDomReady, 50);
       return;
     }
-    if (!f) {
-      f = true;
-      _();
-      g(r, function (e) {
-        S(e);
-      });
+
+    if (!domReady) {
+      domReady = true;
+      checkAutoload();
+      each(domReadyCallbacks, done);
     }
   }
-  function H() {
-    if (n.addEventListener) {
-      n.removeEventListener("DOMContentLoaded", H, false);
-      P();
-    } else if (n.readyState === "complete") {
-      n.detachEvent("onreadystatechange", H);
-      P();
+
+  function handleDOMContentLoaded() {
+    if (document.addEventListener) {
+      document.removeEventListener("DOMContentLoaded", handleDOMContentLoaded);
+      onDomReady();
+    } else if (document.readyState === "complete") {
+      document.detachEvent("onreadystatechange", handleDOMContentLoaded);
+      onDomReady();
     }
   }
-  var n = e.document,
-    r = [],
-    i = [],
-    s = {},
-    o = {},
-    u =
-      "async" in n.createElement("script") ||
-      "MozAppearance" in n.documentElement.style ||
-      e.opera,
-    a,
-    f,
-    l = (e.head_conf && e.head_conf.head) || "head",
-    c = (e[l] =
-      e[l] ||
-      function () {
-        c.ready.apply(null, arguments);
-      }),
-    h = 1,
-    p = 2,
-    d = 3,
-    v = 4;
-  if (n.readyState === "complete") {
-    P();
-  } else if (n.addEventListener) {
-    n.addEventListener("DOMContentLoaded", H, false);
-    e.addEventListener("load", P, false);
+
+  const document = global.document;
+  const domReadyCallbacks = [];
+  const preloadQueue = [];
+  const readyCallbacks = {};
+  const resources = {};
+
+  const supportsAsync =
+    "async" in document.createElement("script") ||
+    "MozAppearance" in document.documentElement.style ||
+    global.opera;
+
+  let initialized = false;
+  let domReady = false;
+
+  const STATE = {
+    PRELOADING: 1,
+    PRELOADED: 2,
+    LOADING: 3,
+    LOADED: 4,
+  };
+
+  const headVar = (global.head_conf && global.head_conf.head) || "head";
+  const loader = (global[headVar] = global[headVar] || function () {
+    loader.ready.apply(null, arguments);
+  });
+
+  // Setup load strategies
+  loader.load = loader.js = supportsAsync ? loadMultiple : load;
+  loader.test = test;
+  loader.ready = onReady;
+
+  loader.ready(document, function () {
+    if (allLoaded()) {
+      each(readyCallbacks.ALL, done);
+    }
+    if (loader.feature) {
+      loader.feature("domloaded", true);
+    }
+  });
+
+  // DOM ready detection
+  if (document.readyState === "complete") {
+    onDomReady();
+  } else if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", handleDOMContentLoaded);
+    global.addEventListener("load", onDomReady);
   } else {
-    n.attachEvent("onreadystatechange", H);
-    e.attachEvent("onload", P);
-    var B = false;
+    document.attachEvent("onreadystatechange", handleDOMContentLoaded);
+    global.attachEvent("onload", onDomReady);
+
+    let top = false;
     try {
-      B = !e.frameElement && n.documentElement;
-    } catch (j) {}
-    if (B && B.doScroll) {
-      (function F() {
-        if (!f) {
+      top = !global.frameElement && document.documentElement;
+    } catch (err) {}
+
+    if (top && top.doScroll) {
+      (function check() {
+        if (!domReady) {
           try {
-            B.doScroll("left");
-          } catch (t) {
-            e.clearTimeout(c.readyTimeout);
-            c.readyTimeout = e.setTimeout(F, 50);
+            top.doScroll("left");
+          } catch (err) {
+            clearTimeout(loader.readyTimeout);
+            loader.readyTimeout = setTimeout(check, 50);
             return;
           }
-          P();
+          onDomReady();
         }
       })();
     }
   }
-  c.load = c.js = u ? A : L;
-  c.test = x;
-  c.ready = D;
-  c.ready(n, function () {
-    if (N()) {
-      g(s.ALL, function (e) {
-        S(e);
-      });
-    }
-    if (c.feature) {
-      c.feature("domloaded", true);
-    }
-  });
-  setTimeout(function () {
-    a = true;
-    g(i, function (e) {
-      e();
-    });
+
+  // Deferred execution
+  setTimeout(() => {
+    initialized = true;
+    each(preloadQueue, (fn) => fn());
   }, 300);
+
 })(window);
