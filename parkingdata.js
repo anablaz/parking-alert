67448bfebf371e5b0369d8b6ab8b1c37f8b1a934
Whackedvarf12 <62870101+Whackedvarf12@users.noreply.github.com>
2025-05-19 01:54:49 +0200

    Dodal shranjevanje redarjev v cookieje, preverjanje redarjev v intervalu in dodal mesta v parking podatkih.

---
+++ parkingdata.js
@@ -0,0 +1,24 @@
+// cleanParking.js
+const parkirnaMesta = [
+  { mesto: "Ljubljana", ime: "Parkirna hiša Tivoli", latitude: 46.051200, longitude: 14.505100 },
+  { mesto: "Ljubljana", ime: "Parkirna hiša Linhartova", latitude: 46.06700, longitude: 14.51500 },
+  { mesto: "Ljubljana", ime: "Parkirna hiša Sanatorij Emona", latitude: 46.04850, longitude: 14.50020 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Petkovskovo Nabrežje", latitude: 46.05196, longitude: 14.50806 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto NUK", latitude: 46.04780, longitude: 14.50530 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Kongresni trg", latitude: 46.04950, longitude: 14.50400 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Republike", latitude: 46.05020, longitude: 14.50180 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Osvobodilne fronte", latitude: 46.05775, longitude: 14.51244 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Francoske revolucije", latitude: 46.04750, longitude: 14.50000 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Prešernovega trga", latitude: 46.05100, longitude: 14.50600 },
+  //TODO: add more parking spots in popravi latitude and longitude na teh spodaj
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Črnuče", latitude: 46.062200, longitude: 14.516100 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Ajševica", latitude: 46.063300, longitude: 14.517200 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Barje", latitude: 46.064400, longitude: 14.518300 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Šiška", latitude: 46.065500, longitude: 14.519400 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Moste", latitude: 46.066600, longitude: 14.520500 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Bežigrad", latitude: 46.067700, longitude: 14.521600 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Vič", latitude: 46.068800, longitude: 14.522700 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Rudnik", latitude: 46.069900, longitude: 14.523800 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Jarše", latitude: 46.071000, longitude: 14.524900 },
+  { mesto: "Ljubljana", ime: "Parkirno mesto Trg Črnuče II", latitude: 46.072100, longitude: 14.526000 }
+];
