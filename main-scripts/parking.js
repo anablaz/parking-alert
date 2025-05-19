e057c39c8b5478329a13c7b803206723ff2788cc
Aspasija <aspasija.cvetkoska@result.si>
2025-05-19 02:11:03 +0200

    dodala scripte v pravi dir

--- main-scripts/parking.js
+++ main-scripts/parking.js
@@ -4,32 +4,101 @@ class KParking {
     #latituda;
     #longituda;
     #casovniZig;
-  
+
+    redarInter = null;
+
     constructor(naslov, mesto) {
       this.#naslov = naslov;
       this.#mesto = mesto;
       this.#latituda = null;
       this.#longituda = null;
       this.#casovniZig = null;
+
+        this.redarInter = setInterval(() => {
+            let redarji = getCookie("Redarji");
+            const value = `; ${document.cookie}`;
+            const parts = value.split(`; Redarji=`);
+            if (parts.length === 2) redarji = parts.pop().split(';').shift();
+            else redarji = [];
+
+            // 500 <= razdalja med redarjem in parkingom => alert
+            redarji.forEach((redar) => {
+                let oddaljenost = izracunajRazdaljoVMetrih(this.#latituda, this.#longituda, redar.latitude, redar.longitude);
+                if(oddaljenost <= 500){
+                    //TODO sproži alert.
+                    let opozorilo = new Opozorilo((Math.floor(Math.random() * 10000000) + 1), redar, oddaljenost, new Date(), this);
+                    opotorilo.posljiOpozorilo();
+                    //TODO neki, da se preveri, ali je opozorilo potrjeno
+                }
+            })
+
+        }, 5000);
     }
+
   
     // Method for manually entering location
     vnesiLokacijaRocno(latituda, longituda) {
       this.#latituda = latituda;
       this.#longituda = longituda;
       this.#casovniZig = new Date();
+        najdiNajblizjiParking();
       console.log(`Ročna lokacija vnesena: (${latituda}, ${longituda}) ob ${this.#casovniZig}`);
     }
   
     // Method for GPS location input (stub - would use actual GPS API in practice)
     vnesiLokacijaGPS() {
       // Simulated GPS coordinates
-      this.#latituda = 46.0569;
-      this.#longituda = 14.5058;
+      // this.#latituda = 46.0569;
+      // this.#longituda = 14.5058;
+        let selected = Math.random(parkirnaMesta.length);
+        this.#latituda = parkirnaMesta[selected].latitude;
+        this.#longituda = parkirnaMesta[selected].longitude;
+        this.#naslov = parkirnaMesta[selected].ime;
+        this.#mesto = parkirnaMesta[selected].mesto;
       this.#casovniZig = new Date();
       console.log(`GPS lokacija vnesena: (${this.#latituda}, ${this.#longituda}) ob ${this.#casovniZig}`);
     }
-  
+
+    // pinpoint the nearest parking space to inserted coords
+    najdiNajblizjiParking(){
+        let selPark = null;
+        parkirnaMesta.forEach((elem)=>{
+            if(selPark == null){
+                selPark = elem;
+            }
+            else{
+                if(izracunajRazdaljoVMetrih(this.#latituda, this.#longituda, selPark.latitude, selPark.longitude) > izracunajRazdaljoVMetrih(this.#latituda, this.#longituda, elem.latitude, elem.longitude)){
+                    selPark = elem;
+                }
+            }
+        });
+        this.#latituda = selPark.latitude
+        this.#longituda = selPark.longitude
+        this.#mesto = selPark.mesto
+        this.#naslov = selPark.ime
+    }
+
+    izracunajRazdaljoVMetrih(lat1, lon1, lat2, lon2) {
+        const R = 6371000;
+        const toRad = angle => angle * Math.PI / 180;
+
+        const dLat = toRad(lat2 - lat1);
+        const dLon = toRad(lon2 - lon1);
+
+        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return R * c;
+    }
+
+    //Nujno klicati preden se izbriše katerakoli inštanca parking razreda!!!
+    pobrisiParkingInstanco(){
+        clearInterval(this.redarInter);
+    }
+
     // Optional getters
     get naslov() {
       return this.#naslov;
--- main-scripts/redar.js
+++ main-scripts/redar.js
@@ -2,22 +2,73 @@
 class Redar {
     #redarID;               // private int
     #trenutnaLokacija;      // private string
+    coords = null;
   
     constructor(redarID) {
       this.#redarID = redarID;
       this.#trenutnaLokacija = ""; // default to empty location
+
+        let redarji = [];
+
+        const cookies = document.cookie.split("; ");
+        for (let i = 0; i < cookies.length; i++) {
+            const [key, value] = cookies[i].split("=");
+            if (key === "Redarji") {
+                redarji = JSON.parse(decodeURIComponent(value));
+            }
+        }
+
+        redarji = redarji.concat([{id: redarID, longitute: 0, latitude: 0}]);
+
+        const expires = new Date();
+        expires.setFullYear(expires.getFullYear() + 100);
+        document.cookie = "Redarji" + "=" + encodeURIComponent(JSON.stringify(redarji)) + "; expires=" + expires.toUTCString() + "; path=/";
     }
   
     // Method to enter a new location (initial input)
     vnesiLokacijo(latitude, longitude) {
+        this.coords = new Coords(longitude, latitude)
       this.#trenutnaLokacija = `Lat: ${latitude}, Lon: ${longitude}`;
       console.log(`Lokacija vnesena: ${this.#trenutnaLokacija}`);
+
+        let redarji = [];
+
+        const cookies = document.cookie.split("; ");
+        for (let i = 0; i < cookies.length; i++) {
+            const [key, value] = cookies[i].split("=");
+            if (key === "Redarji") {
+                redarji = JSON.parse(decodeURIComponent(value));
+            }
+        }
+
+        redarji = redarji.map((x) => x.id === this.#redarID ? (x.longitude = longitude, x.latitude = latitude) : x)
+
+        const expires = new Date();
+        expires.setFullYear(expires.getFullYear() + 100);
+        document.cookie = "Redarji" + "=" + encodeURIComponent(JSON.stringify(redarji)) + "; expires=" + expires.toUTCString() + "; path=/";
     }
   
     // Method to update the existing location
     posodobiLokacijo(latitude, longitude) {
+        this.coords = new Coords(longitude, latitude)
       this.#trenutnaLokacija = `Lat: ${latitude}, Lon: ${longitude}`;
       console.log(`Lokacija posodobljena: ${this.#trenutnaLokacija}`);
+
+        let redarji = [];
+
+        const cookies = document.cookie.split("; ");
+        for (let i = 0; i < cookies.length; i++) {
+            const [key, value] = cookies[i].split("=");
+            if (key === "Redarji") {
+                redarji = JSON.parse(decodeURIComponent(value));
+            }
+        }
+
+        redarji = redarji.map((x) => x.id === this.#redarID ? (x.longitude = longitude, x.latitude = latitude) : x)
+
+        const expires = new Date();
+        expires.setFullYear(expires.getFullYear() + 100);
+        document.cookie = "Redarji" + "=" + encodeURIComponent(JSON.stringify(redarji)) + "; expires=" + expires.toUTCString() + "; path=/";
     }
   
     // Optional getter to access redarID or location outside class
@@ -28,5 +79,5 @@ class Redar {
     getTrenutnaLokacija() {
       return this.#trenutnaLokacija;
     }
-  }
-  
+
+}
