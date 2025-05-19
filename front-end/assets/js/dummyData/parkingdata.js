7c6bad73983af40129b9a12a57992ddbdb0a220b
Aspasija <aspasija.cvetkoska@result.si>
2025-05-19 13:11:59 +0200

    dodatna parkirna mesta

--- front-end/assets/js/dummyData/parkingdata.js
+++ front-end/assets/js/dummyData/parkingdata.js
@@ -10,18 +10,25 @@ export const parkirnaMesta = [
   { ime: "Parkirno mesto Trg Osvobodilne fronte", latitude: 46.05775, longitude: 14.51244 },
   { ime: "Parkirno mesto Trg Francoske revolucije", latitude: 46.04750, longitude: 14.50000 },
   { ime: "Parkirno mesto Trg Prešernovega trga", latitude: 46.05100, longitude: 14.50600 },
-  //TODO: add more parking spots in popravi latitude and longitude na teh spodaj
-  { ime: "Parkirno mesto Trg Črnuče", latitude: 46.062200, longitude: 14.516100 },
-  { ime: "Parkirno mesto Trg Ajševica", latitude: 46.063300, longitude: 14.517200 },
-  { ime: "Parkirno mesto Trg Barje", latitude: 46.064400, longitude: 14.518300 },
-  { ime: "Parkirno mesto Trg Šiška", latitude: 46.065500, longitude: 14.519400 },
-  { ime: "Parkirno mesto Trg Moste", latitude: 46.066600, longitude: 14.520500 },
-  { ime: "Parkirno mesto Trg Bežigrad", latitude: 46.067700, longitude: 14.521600 },
-  { ime: "Parkirno mesto Trg Vič", latitude: 46.068800, longitude: 14.522700 },
-  { ime: "Parkirno mesto Trg Rudnik", latitude: 46.069900, longitude: 14.523800 },
-  { ime: "Parkirno mesto Trg Jarše", latitude: 46.071000, longitude: 14.524900 },
-  { ime: "Parkirno mesto Trg Črnuče II", latitude: 46.072100, longitude: 14.526000 }
+  { ime: "Parkirno mesto Trg Črnuče", latitude: 46.103000, longitude: 14.540000 },
+  { ime: "Parkirno mesto Trg Ajševica", latitude: 46.043200, longitude: 14.470100 },
+  { ime: "Parkirno mesto Trg Barje", latitude: 46.015500, longitude: 14.487000 },
+  { ime: "Parkirno mesto Trg Šiška", latitude: 46.072000, longitude: 14.489000 },
+  { ime: "Parkirno mesto Trg Moste", latitude: 46.056500, longitude: 14.538900 },
+  { ime: "Parkirno mesto Trg Bežigrad", latitude: 46.074200, longitude: 14.521500 },
+  { ime: "Parkirno mesto Trg Vič", latitude: 46.033800, longitude: 14.482900 },
+  { ime: "Parkirno mesto Trg Rudnik", latitude: 46.020200, longitude: 14.523300 },
+  { ime: "Parkirno mesto Trg Jarše", latitude: 46.068900, longitude: 14.552200 },
+  { ime: "Parkirno mesto Trg Črnuče II", latitude: 46.105200, longitude: 14.536600 },
+  // Novo dodani
+  { ime: "Parkirno mesto Brdo", latitude: 46.049700, longitude: 14.458200 },
+  { ime: "Parkirno mesto Stožice", latitude: 46.080100, longitude: 14.526800 },
+  { ime: "Parkirno mesto Zalog", latitude: 46.061300, longitude: 14.574200 },
+  { ime: "Parkirno mesto Rakova Jelša", latitude: 46.026500, longitude: 14.511400 },
+  { ime: "Parkirno mesto Polje", latitude: 46.056700, longitude: 14.584300 },
+  { ime: "Parkirno mesto Rožna dolina", latitude: 46.042600, longitude: 14.479000 },
+  { ime: "Parkirno mesto Murgle", latitude: 46.029900, longitude: 14.494200 },
+  { ime: "Parkirno mesto Ježica", latitude: 46.093800, longitude: 14.507700 },
+  { ime: "Parkirno mesto BTC City", latitude: 46.065000, longitude: 14.546000 },
+  { ime: "Parkirno mesto Žale", latitude: 46.071200, longitude: 14.520300 }
 ];
-
-
-
